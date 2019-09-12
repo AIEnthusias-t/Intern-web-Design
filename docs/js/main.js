8bd7c6b2506384bb1463d7691c5c1db29f1f4dec
jitin <lambajitin.24@gmail.com>
2019-09-12 15:44:31 +0530

    attempt to design responsive webpage

--- docs/js/main.js
+++
@@ -1,6 +0,0 @@
-const $trigger = document.querySelector('.js-mobile')
-const nav = document.querySelector('.js-nav')
-
-$trigger.addEventListener('click', function() {
-  nav.classList.toggle('site-header__list--show')
-})
